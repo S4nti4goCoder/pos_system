c046ea6faef2bba9595f592abd2c9f432100e85a
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-03-28 19:00:46 -0500

    feat: Sub-Modules are created in the Product Module.

--- cms/views/pages/dynamic/custom/products/products.php
+++ cms/views/pages/dynamic/custom/products/products.php
@@ -7,61 +7,26 @@ Custom
 	<?php if ($_SESSION["admin"]->rol_admin == "superadmin"): ?>
 
 		<div class="position-absolute border rounded" style="top:0px; right:12px; z-index:100">
-			
+
 			<button type="button" class="btn btn-sm text-muted rounded m-0 px-1 py-0 border-0 myModule" item='<?php echo json_encode($module) ?>' idPage="<?php echo $page->results[0]->id_page ?>">
 				<i class="bi bi-pencil-square"></i>
 			</button>
 
-			<button type="button" class="btn btn-sm text-muted rounded m-0 px-1 py-0 border-0 deleteModule" idModule=<?php echo base64_encode($module->id_module) ?> >
+			<button type="button" class="btn btn-sm text-muted rounded m-0 px-1 py-0 border-0 deleteModule" idModule=<?php echo base64_encode($module->id_module) ?>>
 				<i class="bi bi-trash"></i>
 			</button>
 
 
 		</div>
-		
+
 	<?php endif ?>
 
 	<!--==============================
-   Start Custom
-  ================================-->
-
-  <div class="card rounded">
-  	
-  	 <div class="card-header">
-        <h3 class="card-title">Progress bars</h3>
-      </div>
-
-      <div class="card-body">
-
-      	<?php for ($i = 0; $i < 4; $i++): ?>
-
-      		<div class="progress mb-3">
-      			<div class="progress-bar bg-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-      				<span class="sr-only">40% Complete (success)</span>
-      			</div>
-      		</div>
-      		<div class="progress mb-3">
-      			<div class="progress-bar bg-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-      				<span class="sr-only">20% Complete</span>
-      			</div>
-      		</div>
-      		<div class="progress mb-3">
-      			<div class="progress-bar bg-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-      				<span class="sr-only">60% Complete (warning)</span>
-      			</div>
-      		</div>
-      		<div class="progress mb-3">
-      			<div class="progress-bar bg-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-      				<span class="sr-only">80% Complete</span>
-      			</div>
-      		</div>
-
-      	<?php endfor ?>
-
-      </div>
-
-
-  </div>
-
-
+   		Start Custom
+  	================================-->
+	<?php
+	include "modules/categories.php";
+	include "modules/search.php";
+	include "modules/catalog.php";
+	?>
 </div>
