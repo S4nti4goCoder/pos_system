bb101107dff7cb3718a6a506c2f04669066717a0
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-04-03 16:12:52 -0500

    feat: Daily Sales Chart.

--- cms/views/pages/dynamic/graphics/graphics.php
+++ cms/views/pages/dynamic/graphics/graphics.php
@@ -1,40 +1,33 @@
-<?php 
+<?php
 
 $xAxis = array();
 $yAxis = array();
 
 $content = json_decode($module->content_module);
 
-$url = $content->table."?select=".$content->xAxis.",".$content->yAxis;
+if ($module->title_module == "gráfico de ventas diarias" && $module->id_page_module == 13) {
+	$url = $content->table . "?linkTo=status_order&equalTo=Completada&select=" . $content->xAxis . "," . $content->yAxis;
+} else {
+	$url = $content->table . "?select=" . $content->xAxis . "," . $content->yAxis;
+}
 $method = "GET";
 $fields = array();
 
-$response = CurlController::request($url,$method,$fields);
-
-if($response->status == 200){
-
+$response = CurlController::request($url, $method, $fields);
+if ($response->status == 200) {
 	$graphic = $response->results;
-
-	foreach (json_decode(json_encode($graphic),true) as $index => $item) {
-
+	foreach (json_decode(json_encode($graphic), true) as $index => $item) {
 		array_push($xAxis, $item[$content->xAxis]);
 		$yAxis[$item[$content->xAxis]] = 0;
-		
 	}
 
 	$xAxis = array_values(array_unique($xAxis));
-
-	foreach (json_decode(json_encode($graphic),true) as $index => $item) {
-		
-		for($i = 0; $i < count($xAxis); $i++){
-
-			if($xAxis[$i] == $item[$content->xAxis]){
-
+	foreach (json_decode(json_encode($graphic), true) as $index => $item) {
+		for ($i = 0; $i < count($xAxis); $i++) {
+			if ($xAxis[$i] == $item[$content->xAxis]) {
 				$yAxis[$item[$content->xAxis]] +=  $item[$content->yAxis];
-				
 			}
 		}
-
 	}
 }
 
@@ -45,29 +38,29 @@ if($response->status == 200){
 	<?php if ($_SESSION["admin"]->rol_admin == "superadmin"): ?>
 
 		<div class="position-absolute border rounded bg-white" style="top:0px; right:12px; z-index:100">
-			
+
 			<button type="button" class="btn btn-sm text-muted rounded m-0 px-1 py-0 border-0 myModule" item='<?php echo json_encode($module) ?>' idPage="<?php echo $page->results[0]->id_page ?>">
 				<i class="bi bi-pencil-square"></i>
 			</button>
 
-			<button type="button" class="btn btn-sm text-muted rounded m-0 px-1 py-0 border-0 deleteModule" idModule=<?php echo base64_encode($module->id_module) ?> >
+			<button type="button" class="btn btn-sm text-muted rounded m-0 px-1 py-0 border-0 deleteModule" idModule=<?php echo base64_encode($module->id_module) ?>>
 				<i class="bi bi-trash"></i>
 			</button>
 
 
 		</div>
-		
+
 	<?php endif ?>
 
-	
+
 	<div class="card rounded">
-		
+
 		<div class="card-header bg-white rounded-top h4 font-weight-bold text-capitalize py-3">
 			<?php echo $module->title_module ?>
 		</div>
 
 		<div class="card-body p-4">
-			<canvas id="chart-<?php echo str_replace(" ","_",$module->title_module) ?>" height="500"></canvas>
+			<canvas id="chart-<?php echo str_replace(" ", "_", $module->title_module) ?>" height="500"></canvas>
 		</div>
 
 	</div>
@@ -75,101 +68,94 @@ if($response->status == 200){
 </div>
 
 <script>
-	
-if($("#chart-<?php echo str_replace(" ","_",$module->title_module) ?>").length > 0){
+	if ($("#chart-<?php echo str_replace(" ", "_", $module->title_module) ?>").length > 0) {
 
-	var graphicChart = $("#chart-<?php echo str_replace(" ","_",$module->title_module) ?>");
-	var tagsChart = new Chart(graphicChart, {
+		var graphicChart = $("#chart-<?php echo str_replace(" ", "_", $module->title_module) ?>");
+		var tagsChart = new Chart(graphicChart, {
 
-		type: "<?php echo $content->type ?>",
-		data: {
-			labels:[
+			type: "<?php echo $content->type ?>",
+			data: {
+				labels: [
 
-				<?php 
+					<?php
 
-					foreach ($xAxis as $index => $item){
+					foreach ($xAxis as $index => $item) {
 
-						echo "'".$item."',";
-
+						echo "'" . $item . "',";
 					}
 
-				?>
+					?>
 
 				],
-			datasets:[
-				{
+				datasets: [{
 					backgroundColor: 'rgba(<?php echo $content->color ?>,.55)',
 					borderColor: 'rgb(<?php echo $content->color ?>)',
 					data: [
 
-						<?php 
+						<?php
 
-							foreach ($xAxis as $index => $item){
+						foreach ($xAxis as $index => $item) {
 
-								echo "'".$yAxis[$item]."',";
-
-							}
+							echo "'" . $yAxis[$item] . "',";
+						}
 
 						?>
 
 
 					]
-				}
-			]
-		},//close data
-		options: {
-	        maintainAspectRatio: false,
-	        tooltips: {
-	          mode: 'index',
-	          intersect: true
-	        },
-	        hover: {
-	          mode: 'index',
-	          intersect: true
-	        },
-	        legend: {
-	          display: false
-	        },
-	        scales: {
-	        	yAxes: [{
-        		 	display: true,
-		            gridLines: {
-		              display: true
-		            },
-		            ticks: $.extend({
-         			  beginAtZero: true,
-		              // Include a dollar sign in the ticks
-		              callback: function (value) {
-		                if (value >= 1000) {
-		                  value /= 1000
-		                  value += 'k'
-		                }
+				}]
+			}, //close data
+			options: {
+				maintainAspectRatio: false,
+				tooltips: {
+					mode: 'index',
+					intersect: true
+				},
+				hover: {
+					mode: 'index',
+					intersect: true
+				},
+				legend: {
+					display: false
+				},
+				scales: {
+					yAxes: [{
+						display: true,
+						gridLines: {
+							display: true
+						},
+						ticks: $.extend({
+							beginAtZero: true,
+							// Include a dollar sign in the ticks
+							callback: function(value) {
+								if (value >= 1000) {
+									value /= 1000
+									value += 'k'
+								}
 
-		                return  value
-		              }
-		            }, 
-		            {
-	                  fontColor: '#495057',
-	                  fontStyle: 'bold'
-            		})
+								return value
+							}
+						}, {
+							fontColor: '#495057',
+							fontStyle: 'bold'
+						})
 
-            	}],
-            	xAxes: [{
-		            display: true,
-		            gridLines: {
-		              display: true
-		            },
-		            ticks: {
-	                  fontColor: '#495057',
-	                  fontStyle: 'bold'
-	                }
-	          	}]
+					}],
+					xAxes: [{
+						display: true,
+						gridLines: {
+							display: true
+						},
+						ticks: {
+							fontColor: '#495057',
+							fontStyle: 'bold'
+						}
+					}]
 
-	        }//close scales
+				} //close scales
 
-	    }//close options
+			} //close options
 
-	})
-}
-
+		})
+	}
 </script>
--- database/pos_system_db.sql
+++ database/pos_system_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-04-2025 a las 07:57:52
+-- Tiempo de generación: 03-04-2025 a las 23:12:05
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -54,7 +54,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id_admin`, `email_admin`, `password_admin`, `rol_admin`, `permissions_admin`, `token_admin`, `token_exp_admin`, `status_admin`, `title_admin`, `symbol_admin`, `font_admin`, `color_admin`, `back_admin`, `scode_admin`, `name_admin`, `id_office_admin`, `chatgpt_admin`, `date_created_admin`, `date_updated_admin`) VALUES
-(1, 'superadmin@pos.com', '', 'superadmin', '{\"todo\":\"on\"}', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDM2MzE4NDUsImV4cCI6MTc0MzcxODI0NSwiZGF0YSI6eyJpZCI6MSwiZW1haWwiOiJzdXBlcmFkbWluQHBvcy5jb20ifX0.OXAiNFl3Iq9jI3bWfSNHUf4_EbrS0sndVgXedKf7wAc', '1743718245', 1, 'POSify', '<i class=\"bi bi-cart-check-fill\"></i>', '<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"><link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin><link href=\"https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap\" rel=\"stylesheet\">', '#00a6fb', 'http://cms.pos.com/views/assets/files/67aeeca5d9fed33.jpg', '', 'El Programador', 0, NULL, '2025-02-14', '2025-04-02 22:10:45'),
+(1, 'superadmin@pos.com', '', 'superadmin', '{\"todo\":\"on\"}', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDM3MTAyMTksImV4cCI6MTc0Mzc5NjYxOSwiZGF0YSI6eyJpZCI6MSwiZW1haWwiOiJzdXBlcmFkbWluQHBvcy5jb20ifX0.ocd05sgQ9XrMtpCgBslDYqPaONtJRRBVUVXW66a-zTI', '1743796619', 1, 'POSify', '<i class=\"bi bi-cart-check-fill\"></i>', '<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"><link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin><link href=\"https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap\" rel=\"stylesheet\">', '#00a6fb', 'http://cms.pos.com/views/assets/files/67aeeca5d9fed33.jpg', '', 'El Programador', 0, NULL, '2025-02-14', '2025-04-03 19:56:59'),
 (2, 'admin@pos.com', '', 'admin', '{\"todo\":\"on\"}', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDMxNDA0MDcsImV4cCI6MTc0MzIyNjgwNywiZGF0YSI6eyJpZCI6MiwiZW1haWwiOiJhZG1pbkBwb3MuY29tIn19.h-kOkv9mNhUhup37tQpJSTovwT-tUcCStC_SAcbut_U', '1743226807', 1, '', '', '', '', '', '', 'Sara Perez', 0, NULL, '2025-03-27', '2025-03-28 05:47:01'),
 (3, 'supervisor@pos.com', '', 'editor', '{\"posify\":\"on\",\"clientes\":\"on\",\"productos\":\"on\",\"compras\":\"on\"}', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDMxNDA2NjMsImV4cCI6MTc0MzIyNzA2MywiZGF0YSI6eyJpZCI6MywiZW1haWwiOiJzdXBlcnZpc29yQHBvcy5jb20ifX0.heafrQdOV-CWa0OgEUpfavjKREHmWZ67CP9b_gjFEus', '1743227063', 1, '', '', '', '', '', '', 'Jorge Riquelme', 0, NULL, '2025-03-28', '2025-03-28 05:46:43'),
 (4, 'admin@pueblolindo.com', '$2a$07$azybxcags23425sdg23sdeanQZqjaf6Birm2NvcYTNtJw24CsO5uq', 'admin', '%7B%22todo%22%3A%22on%22%7D', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDMxOTY2MTgsImV4cCI6MTc0MzI4MzAxOCwiZGF0YSI6eyJpZCI6NCwiZW1haWwiOiJhZG1pbkBwdWVibG9saW5kby5jb20ifX0.32Tm3M7j5xxqzC64MYpZCGBjbZJg-OOxAE66-gjDXik', '1743283018', 1, '', '', '', '', '', '', 'Jaime+Suarez', 1, NULL, '2025-03-28', '2025-03-28 21:16:58'),
@@ -122,7 +122,9 @@ CREATE TABLE `cashs` (
 INSERT INTO `cashs` (`id_cash`, `start_cash`, `bills_cash`, `money_cash`, `diff_cash`, `end_cash`, `gap_cash`, `status_cash`, `date_start_cash`, `date_end_cash`, `id_admin_cash`, `id_office_cash`, `date_created_cash`, `date_updated_cash`) VALUES
 (1, 1000, 0, 0, 0, 0, 0, 0, '2025-04-02 18:03:00', '0000-00-00 00:00:00', 1, 1, '2025-04-03', '2025-04-03 03:53:24'),
 (2, 1000, 0, 0, 0, 0, 0, 0, '2025-04-02 22:03:00', '0000-00-00 00:00:00', 1, 1, '2025-04-02', '2025-04-03 05:23:23'),
-(3, 1000, -60, 5118.33, 6058.33, 6000, -58.33, 0, '2025-04-03 00:03:00', '2025-04-03 00:42:00', 1, 1, '2025-04-03', '2025-04-03 05:42:51');
+(3, 1000, -60, 5118.33, 6058.33, 6000, -58.33, 0, '2025-04-03 00:03:00', '2025-04-03 00:42:00', 1, 1, '2025-04-03', '2025-04-03 05:42:51'),
+(4, 10000, -60, 8411.8, 18351.8, 18351.8, 0, 0, '2025-04-03 01:03:00', '2025-04-03 14:16:00', 1, 1, '2025-04-03', '2025-04-03 19:16:13'),
+(5, 1000, -60, 8840.2, 9780.2, 9780.2, 0, 0, '2025-04-03 14:03:00', '2025-04-03 14:21:00', 1, 1, '2025-04-03', '2025-04-03 19:21:09');
 
 -- --------------------------------------------------------
 
@@ -416,7 +418,12 @@ INSERT INTO `modules` (`id_module`, `id_page_module`, `type_module`, `title_modu
 (20, 12, 'tables', 'bills', 'bill', '', 100, 1, '2025-02-22', '2025-02-22 05:07:19'),
 (21, 1, 'custom', 'orders', '', '', 100, 1, '2025-03-29', '2025-03-28 23:45:34'),
 (22, 1, 'custom', 'products', '', '', 50, 1, '2025-03-29', '2025-03-28 23:48:48'),
-(23, 1, 'custom', 'panel', '', '', 50, 1, '2025-03-29', '2025-03-28 23:49:09');
+(23, 1, 'custom', 'panel', '', '', 50, 1, '2025-03-29', '2025-03-28 23:49:09'),
+(24, 13, 'metrics', 'ventas', '', '{\"type\":\"add\",\"table\":\"orders\", \"column\":\"total_order\",\"config\":\"price\",\"icon\":\"fas fa-cart-arrow-down\",\"color\":\"28, 175, 159\"  }', 25, 1, '2025-04-03', '2025-04-03 20:02:03'),
+(25, 13, 'metrics', 'compras', '', '{\"type\":\"add\",\"table\":\"purchases\", \"column\":\"invest_purchase\",\"config\":\"price\",\"icon\":\"fas fa-shopping-basket\",\"color\":\"128, 0, 0\"  }', 25, 1, '2025-04-03', '2025-04-03 20:07:04'),
+(26, 13, 'metrics', 'productos', '', '{\"type\":\"add\",\"table\":\"products\", \"column\":\"stock_product\",\"config\":\"unit\",\"icon\":\"fas fa-box\",\"color\":\"77, 93, 219\"  }', 25, 1, '2025-04-03', '2025-04-03 20:08:20'),
+(27, 13, 'metrics', 'clientes', '', '{\"type\":\"total\",\"table\":\"clients\", \"column\":\"id_client\",\"config\":\"unit\",\"icon\":\"fas fa-users\",\"color\":\"43, 62, 101\"  }', 25, 1, '2025-04-03', '2025-04-03 20:09:23'),
+(28, 13, 'graphics', 'gráfico de ventas diarias', '', '{\"type\":\"bar\",\"table\":\"orders\",\"xAxis\":\"date_created_order\",\"yAxis\":\"total_order\",\"color\":\"134, 153, 163\"}', 100, 1, '2025-04-03', '2025-04-03 20:57:30');
 
 -- --------------------------------------------------------
 
@@ -471,9 +478,11 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id_order`, `transaction_order`, `id_admin_order`, `id_client_order`, `subtotal_order`, `discount_order`, `tax_order`, `total_order`, `method_order`, `transfer_order`, `status_order`, `date_order`, `id_office_order`, `date_created_order`, `date_updated_order`) VALUES
-(1, '943143249651', 1, 1, 1398.6, 419.58, 186.01, 1165.03, 'efectivo', '', 'Completada', '2025-04-03 04:42:21', 1, '2025-04-02', '2025-04-03 04:42:21'),
-(2, '981596463137', 1, 1, 0, 0, 0, 0, NULL, NULL, 'Pendiente', '2025-04-03 05:01:59', 1, '2025-04-02', '2025-04-03 05:01:59'),
-(3, '754657799523', 1, 1, 4720.7, 419.58, 817.21, 5118.33, 'efectivo', '', 'Completada', '2025-04-03 05:33:39', 1, '2025-04-03', '2025-04-03 05:33:39');
+(1, '943143249651', 1, 1, 1398.6, 419.58, 186.01, 1165.03, 'efectivo', '', 'Completada', '2025-04-03 21:11:19', 1, '2025-03-30', '2025-04-03 21:11:19'),
+(2, '981596463137', 1, 1, 0, 0, 0, 0, 'null', NULL, 'Pendiente', '2025-04-03 21:11:24', 1, '2025-03-31', '2025-04-03 21:11:24'),
+(3, '754657799523', 1, 1, 4720.7, 419.58, 817.21, 5118.33, 'efectivo', '', 'Completada', '2025-04-01 05:33:39', 1, '2025-04-01', '2025-04-03 20:55:33'),
+(4, '186129524186', 1, 10, 3187.2, 419.58, 525.85, 3293.47, 'efectivo', '', 'Completada', '2025-04-02 06:02:16', 1, '2025-04-02', '2025-04-03 21:04:28'),
+(5, '636374495911', 1, 4, 360, 0, 68.4, 428.4, 'efectivo', '', 'Completada', '2025-04-03 20:54:06', 1, '2025-04-03', '2025-04-03 20:54:06');
 
 -- --------------------------------------------------------
 
@@ -508,7 +517,8 @@ INSERT INTO `pages` (`id_page`, `title_page`, `url_page`, `icon_page`, `type_pag
 (9, 'Órdenes', 'ordenes', 'bi bi-ticket-detailed', 'modules', 9, '2025-02-21', '2025-04-03 01:57:14'),
 (10, 'Ventas', 'ventas', 'bi bi-cash-coin', 'modules', 10, '2025-02-21', '2025-04-03 01:57:14'),
 (11, 'Caja', 'caja', 'fas fa-cash-register', 'modules', 11, '2025-02-22', '2025-04-03 01:57:14'),
-(12, 'Gastos', 'gastos', 'fas fa-money-bill-wave', 'modules', 12, '2025-02-22', '2025-04-03 01:57:14');
+(12, 'Gastos', 'gastos', 'fas fa-money-bill-wave', 'modules', 12, '2025-02-22', '2025-04-03 01:57:14'),
+(13, 'Informes', 'informes', 'bi bi-file-earmark-bar-graph-fill', 'modules', 1000, '2025-04-03', '2025-04-03 19:58:46');
 
 -- --------------------------------------------------------
 
@@ -538,20 +548,20 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id_product`, `title_product`, `img_product`, `id_category_product`, `sku_product`, `unit_product`, `tax_product`, `rte_product`, `stock_product`, `discount_product`, `status_product`, `id_office_product`, `date_created_product`, `date_updated_product`) VALUES
-(1, 'Airpod+2', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b50e59d065857.png', 1, 'PT001', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
-(2, 'Swagme', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b511f3b9d8b19.png', 1, 'PT002', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
+(1, 'Airpod+2', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b50e59d065857.png', 1, 'PT001', 'unidad', 'IVA_19', 'NULL', 99, 0, 1, 1, '2025-02-19', '2025-04-03 06:03:49'),
+(2, 'Swagme', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b511f3b9d8b19.png', 1, 'PT002', 'unidad', 'IVA_19', 'NULL', 99, 0, 1, 1, '2025-02-19', '2025-04-03 06:03:49'),
 (3, 'Red Nike Angelo', 'http://cms.pos.com/views/assets/files/67b51236c169e26.png', 2, 'PT003', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
 (4, 'Blue+White+OGR', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b5126993af317.png', 2, 'PT004', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
 (5, 'Green+Nike+Fe', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b51293ea9fc59.png', 2, 'PT005', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
 (6, 'Iphone+11', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b512bd6039241.png', 3, 'PT006', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
-(7, 'Iphone+14+64GB', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b512e57b1b121.png', 3, 'PT007', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
+(7, 'Iphone+14+64GB', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b512e57b1b121.png', 3, 'PT007', 'unidad', 'IVA_19', 'NULL', 99, 0, 1, 1, '2025-02-19', '2025-04-03 06:03:49'),
 (8, 'Rolex+Tribute+V3', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b5130b951be59.png', 4, 'PT008', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
 (9, 'Timex+Black+Silver', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b513356d92341.png', 4, 'PT009', 'unidad', 'IVA_19', 'NULL', 97, 0, 1, 1, '2025-02-19', '2025-04-03 05:46:43'),
 (10, 'Fossil+Pair+Of+3+in+1', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b51365706b829.png', 4, 'PT0010', 'unidad', 'IVA_19', 'NULL', 98, 0, 1, 1, '2025-02-19', '2025-04-03 05:46:43'),
 (11, 'MacBook+Pro', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b513a1cf59e29.png', 5, 'PT0011', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-03-30 04:10:11'),
 (12, 'IdeaPad+Slim+5+Gen+7', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b513cf3ede415.png', 5, 'PT0012', 'unidad', 'IVA_19', 'NULL', 100, 0, 1, 1, '2025-02-19', '2025-04-03 05:46:43'),
 (13, 'Tablet+1.02+inch', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b513f87186b56.png', 5, 'PT0013', 'unidad', 'IVA_19', 'NULL', 99, 0, 1, 1, '2025-02-19', '2025-04-03 04:59:45'),
-(14, 'Yoga+Book+9i', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b51425c18eb41.png', 5, 'PT0014', 'unidad', 'IVA_19', 'NULL', 98, 30, 1, 1, '2025-02-19', '2025-04-03 04:59:45'),
+(14, 'Yoga+Book+9i', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b51425c18eb41.png', 5, 'PT0014', 'unidad', 'IVA_19', 'NULL', 97, 30, 1, 1, '2025-02-19', '2025-04-03 06:03:50'),
 (15, 'Airpod+2', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b50e59d065857.png', 1, 'PT001', 'unidad', 'IVA_19', 'NULL', 0, 0, 1, 2, '2025-02-19', '2025-02-19 00:23:15'),
 (16, 'Swagme', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b511f3b9d8b19.png', 1, 'PT002', 'unidad', 'IVA_19', 'NULL', 0, 0, 1, 2, '2025-02-19', '2025-02-19 00:23:22'),
 (17, 'Red+Nike+Angelo', 'http%3A%2F%2Fcms.pos.com%2Fviews%2Fassets%2Ffiles%2F67b51236c169e26.png', 2, 'PT003', 'unidad', 'IVA_19', 'NULL', 0, 0, 1, 2, '2025-02-19', '2025-02-19 00:23:28'),
@@ -682,7 +692,14 @@ INSERT INTO `sales` (`id_sale`, `id_order_sale`, `id_product_sale`, `tax_type_sa
 (5, 3, 14, 'IVA', 19, 30, 1, 1398.6, 'Completada', 1, 1, 1, '2025-04-03', '2025-04-03 05:33:40'),
 (6, 3, 13, 'IVA', 19, 0, 1, 1118.6, 'Completada', 1, 1, 1, '2025-04-03', '2025-04-03 05:33:40'),
 (7, 3, 10, 'IVA', 19, 0, 2, 1037.4, 'Completada', 1, 1, 1, '2025-04-03', '2025-04-03 05:33:40'),
-(8, 3, 9, 'IVA', 19, 0, 3, 1166.1, 'Completada', 1, 1, 1, '2025-04-03', '2025-04-03 05:33:40');
+(8, 3, 9, 'IVA', 19, 0, 3, 1166.1, 'Completada', 1, 1, 1, '2025-04-03', '2025-04-03 05:33:40'),
+(9, 4, 14, 'IVA', 19, 30, 1, 1398.6, 'Completada', 1, 10, 1, '2025-04-03', '2025-04-03 06:02:16'),
+(10, 4, 7, 'IVA', 19, 0, 1, 1258.6, 'Completada', 1, 10, 1, '2025-04-03', '2025-04-03 06:02:17'),
+(11, 4, 1, 'IVA', 19, 0, 1, 390, 'Completada', 1, 10, 1, '2025-04-03', '2025-04-03 06:02:17'),
+(12, 4, 2, 'IVA', 19, 0, 1, 140, 'Completada', 1, 10, 1, '2025-04-03', '2025-04-03 06:02:17'),
+(13, 5, 5, 'IVA', 19, 0, 1, 120, 'Completada', 1, 4, 1, '2025-04-03', '2025-04-03 19:19:32'),
+(14, 5, 4, 'IVA', 19, 0, 1, 120, 'Completada', 1, 4, 1, '2025-04-03', '2025-04-03 19:19:32'),
+(15, 5, 3, 'IVA', 19, 0, 1, 120, 'Completada', 1, 4, 1, '2025-04-03', '2025-04-03 19:19:32');
 
 --
 -- Índices para tablas volcadas
@@ -798,7 +815,7 @@ ALTER TABLE `bills`
 -- AUTO_INCREMENT de la tabla `cashs`
 --
 ALTER TABLE `cashs`
-  MODIFY `id_cash` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_cash` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `categories`
@@ -834,7 +851,7 @@ ALTER TABLE `folders`
 -- AUTO_INCREMENT de la tabla `modules`
 --
 ALTER TABLE `modules`
-  MODIFY `id_module` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_module` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `offices`
@@ -846,13 +863,13 @@ ALTER TABLE `offices`
 -- AUTO_INCREMENT de la tabla `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `pages`
 --
 ALTER TABLE `pages`
-  MODIFY `id_page` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_page` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
@@ -870,7 +887,7 @@ ALTER TABLE `purchases`
 -- AUTO_INCREMENT de la tabla `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id_sale` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_sale` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
